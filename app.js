8473124a9447b47347cc4bc31ec833d688e2643b
Pedro de Castilho <mail@pedrodecastilho.com>
2025-02-14 16:56:52 -0300

    define as funcionalidades da aplicacao

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+
+
+/*
+Funcionalidades:
+- Adicionar nomes ao array
+- Validar entrada (campo não pode ser vazio)
+- Visualizar a lista
+- Sortear um nome aleatoriamente na lista
+*/
